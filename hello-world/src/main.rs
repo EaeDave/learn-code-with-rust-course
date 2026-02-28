ed633daad3d2742238c9d619931fa6994d50be91
EaeDave <d4vd.sv@gmail.com>
2026-02-28 19:30:53 -0300

    comments // and multi-line comments /* */

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, World! I am a Rust developer.");
+    // This is a comment, it will be ignored by the rust compiler
+    println!("Hello, World! I am a Rust developer.");  // Comments are important to human context in code
+
+    /*
+     * This is a multi-line
+     * comment in Rust
+     * All these lines will be ignored
+     * by compiler
+     */
 }
